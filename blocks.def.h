6138dc9d8e667c24a366e0f81cc5df84ef62e0c9
Himanshu <himanshugoswami.12feb@gmail.com>
2022-01-01 19:43:01 +0530

    basic dwmblocks build

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"",	"dwm-nettraf",	5,	4},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	20,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" ", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
+/* Here dwm-internet is the name of my script in /usr/bin */
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
